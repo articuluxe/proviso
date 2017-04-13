f9858fb6e41dea9bca193b9d3f6d1116f693f2be
Dan Harms <danielrharms@gmail.com>
2017-04-13 07:59:09 -0500

    add proviso-etags-select

--- proviso-tags.el
+++ proviso-tags.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <enniomore@icloud.com>
 ;; Created: Thursday, January  5, 2017
 ;; Version: 1.0
-;; Modified Time-stamp: <2017-03-30 17:19:16 dharms>
+;; Modified Time-stamp: <2017-04-13 07:58:18 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords: proviso tags
 
@@ -27,6 +27,26 @@
 ;;; Code:
 (require 'proviso-core)
 (require 'tramp)
+(require 'proviso-etags-select)
+
+(defvar proviso-tags-lookup-target-project nil
+  "The active project when a tag is first looked up.")
+
+(defun proviso-tags-store-project ()
+  "Store the current project."
+  (setq proviso-tags-lookup-target-project (symbol-name proviso-local-proj)))
+
+(defun proviso-tags-find-tag ()
+  "Find a tag based on the current profile."
+  (interactive)
+  (proviso-tags-store-project)
+  (etags-select-find-tag))
+
+(defun proviso-tags-find-tag-at-point ()
+  "Find the tag at point based on the current profile."
+  (interactive)
+  (proviso-tags-store-project)
+  (etags-select-find-tag-at-point))
 
 (defun proviso-tags-compute-remote-subdir-stem (proj)
   "Compute remote profile PROJ's stem.
@@ -67,6 +87,31 @@ DIR gives the root directory."
                             root))))))
 
 (add-hook 'proviso-on-project-pre-init 'proviso-tags-on-init)
+
+(defun proviso-etags--real-file-name (filename)
+  "Return the tag's correct destination file for FILENAME.
+This may prepend a remote prefix."
+  (concat
+   (proviso-get proviso-tags-lookup-target-project :remote-prefix)
+   (if (file-name-absolute-p filename)
+       filename
+     ;; todo: used to append 'src-sub-dir here?
+     (proviso-get proviso-tags-lookup-target-project :root-dir))))
+
+;; point etags-select to our function
+(setq etags-select-real-file-name 'proviso-etags--real-file-name)
+
+(defun proviso-etags--insert-file-name(filename tag-file-path)
+  "Return a display name for FILENAME.
+TAG-FILE-PATH is the TAGS file being looked at."
+  (if (file-name-absolute-p filename)
+      filename
+    (concat (proviso-get proviso-tags-lookup-target-project :root-dir)
+            (proviso-get proviso-tags-lookup-target-project :src-sub-dir)
+            filename)))
+
+;; point etags-select to our function
+(setq etags-select-insert-file-name 'proviso-etags--insert-file-name)
 
 (provide 'proviso-tags)
 ;;; proviso-tags.el ends here
---
+++ test/test_proviso-tags.el
@@ -0,0 +1,63 @@
+#!/bin/sh
+":"; exec "$EMACSX" --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; -*-
+;;; test_proviso-tags.el --- test proviso tags functionality
+;; Copyright (C) 2017  Dan Harms (dharms)
+;; Author: Dan Harms <enniomore@icloud.com>
+;; Created: Thursday, April 13, 2017
+;; Version: 1.0
+;; Modified Time-stamp: <2017-04-13 07:57:12 dharms>
+;; Modified by: Dan Harms
+;; Keywords: proviso project tags etags
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(load-file "test/proviso-test-common.el")
+(require 'proviso)
+
+(ert-deftest proviso-tags-test-one ()
+  (proviso-test-reset-all)
+  (let ((base (file-name-directory load-file-name))
+        file-contents)
+    (cl-letf (((symbol-function 'proviso--load-file)
+               (lambda (_)
+                 (proviso-eval-string file-contents))))
+      ;; open file
+      (setq file-contents "
+ (defun do-init (proj)
+   (proviso-put proj :proj-alist
+               '( (:name \"base\" :dir \"\")
+                  )))
+ (proviso-define \"c\" :initfun 'do-init)
+")
+      (find-file (concat base "a/b/c/d/dfile1"))
+      (should (proviso-name-p (proviso-get proviso-local-proj :project-name)))
+      (should (string= (proviso-get proviso-local-proj :root-dir)
+                       (concat base "a/b/c/")))
+      (should (string= (proviso-get proviso-local-proj :project-name)
+                       "c"))
+      (should (equal (get-register ?r) (cons 'file (concat base "a/b/c/"))))
+      (should (equal (get-register ?c) (cons 'file (concat base "a/b/c/"))))
+      (should (equal (get-register ?1) nil))
+      ;; clean up buffers
+      (kill-buffer "dfile1")
+      )))
+
+(ert-run-tests-batch-and-exit (car argv))
+
+;;; test_proviso-tags.el ends here
