c83c8ca1a17b78208092a89cb9c6aae13531b3da
Dan Harms <danielrharms@gmail.com>
2018-02-01 08:53:18 -0600

    add test for proviso-gud

---
+++ test/test_proviso-gud.el
@@ -0,0 +1,94 @@
+#!/bin/sh
+":"; exec "$VISUAL" --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; -*-
+;;; test_proviso-gud.el --- test proviso gud functionality
+;; Copyright (C) 2018  Dan Harms (dharms)
+;; Author: Dan Harms <enniomore@icloud.com>
+;; Created: Thursday, February  1, 2018
+;; Version: 1.0
+;; Modified Time-stamp: <2018-02-01 08:49:33 dharms>
+;; Modified by: Dan Harms
+;; Keywords: tools proviso gud debugger test
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(load-file "test/proviso-test-common.el")
+(require 'proviso)
+
+(ert-deftest proviso-gud-open-empty-project ()
+  (proviso-test-reset-all)
+  (let ((base (file-name-directory load-file-name))
+        file-contents)
+    (cl-letf (((symbol-function 'proviso--load-file)
+               (lambda (_)
+                 (proviso-eval-string file-contents))))
+      ;; open file
+      (setq file-contents "
+ (defun do-init (proj)
+   (proviso-put proj :proj-alist
+               '( (:name \"base\" :dir \"\")
+                  )))
+ (proviso-define-project \"c\" :initfun 'do-init)
+")
+      (find-file (concat base "a/b/c/d/dfile1"))
+      (should (proviso-name-p (proviso-get proviso-local-proj :project-name)))
+      (should (string= (proviso-get proviso-local-proj :root-dir)
+                       (concat base "a/b/c/")))
+      (should (string= (proviso-get proviso-local-proj :project-name)
+                       "c"))
+      (should (equal (proviso-gud-gather-debug-dirs proviso-local-proj)
+                     nil))
+      ;; clean up buffers
+      (kill-buffer "dfile1")
+      )))
+
+(ert-deftest proviso-gud-open-project ()
+  (proviso-test-reset-all)
+  (let ((base (file-name-directory load-file-name))
+        file-contents)
+    (cl-letf (((symbol-function 'proviso--load-file)
+               (lambda (_)
+                 (proviso-eval-string file-contents))))
+      ;; open file
+      (setq file-contents "
+ (defun do-init (proj)
+   (proviso-put proj :proj-alist
+               '( (:name \"base\" :dir \"\")
+                  ))
+   (proviso-put proj :debug-subdirs
+               '( (:name \"DBG\" :dir \"d2\")
+                ))
+   )
+ (proviso-define-project \"c\" :initfun 'do-init)
+")
+      (find-file (concat base "a/b/c/d/dfile1"))
+      (should (proviso-name-p (proviso-get proviso-local-proj :project-name)))
+      (should (string= (proviso-get proviso-local-proj :root-dir)
+                       (concat base "a/b/c/")))
+      (should (string= (proviso-get proviso-local-proj :project-name)
+                       "c"))
+      (should (equal (proviso-gud-gather-debug-dirs proviso-local-proj)
+                     (list (cons (concat base "a/b/c/d2")
+                                 (concat base "a/b/c/d2")))))
+      ;; clean up buffers
+      (kill-buffer "dfile1")
+      )))
+
+(ert-run-tests-batch-and-exit (car argv))
+
+;;; test_proviso-gud.el ends here
