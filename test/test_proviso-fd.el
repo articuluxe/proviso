47761de9cce65af668d0ace37aced5df6bd6acd5
Dan Harms <danielrharms@gmail.com>
2019-10-22 09:08:10 -0500

    some testing helpers for fulledit and fd

---
+++ test/test_proviso-fd.el
@@ -0,0 +1,55 @@
+;;; test_proviso-fd.el --- test proviso-fd
+;; Copyright (C) 2019  Dan Harms (dharms)
+;; Author: Dan Harms <enniomore@icloud.com>
+;; Created: Tuesday, October 22, 2019
+;; Version: 1.0
+;; Modified Time-stamp: <2019-10-22 09:06:58 dharms>
+;; Modified by: Dan Harms
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(require 'proviso-fulledit)
+(require 'proviso-fd)
+
+(defun test-proviso-fd-fulledit (dir)
+  "Get files under DIR."
+  (interactive "DDir: ")
+  (ivy-read "File: "
+            (proviso-finder-adjust-paths
+             (sort
+              (proviso-fulledit-gather-files
+               dir proviso-uninteresting-files
+               proviso-uninteresting-dirs proviso-interesting-files)
+              'string-lessp)
+             nil (file-name-absolute-p dir))))
+
+(defun test-proviso-fd-fd (dir)
+  "Get files under DIR."
+  (interactive "DDir: ")
+  (ivy-read "File: "
+            (proviso-finder-adjust-paths
+             (sort
+              (proviso-fd-gather-files
+               dir nil proviso-uninteresting-files
+               proviso-uninteresting-dirs proviso-interesting-files)
+              'string-lessp)
+             nil (file-name-absolute-p dir))))
+
+;;; test_proviso-fd.el ends here
